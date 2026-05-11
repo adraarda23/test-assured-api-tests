f2c8db630e1103cd116e54f83a2ff8c7c9648e29
adraarda23 <ardaaydinkilinc@gmail.com>
2026-05-12 00:44:53 +0300

    refactor: convert ApiError to a record

--- api/src/main/java/com/ardakilinc/tasksapi/error/ApiError.java
+++ api/src/main/java/com/ardakilinc/tasksapi/error/ApiError.java
@@ -3,32 +3,9 @@ package com.ardakilinc.tasksapi.error;
 import java.time.Instant;
 import java.util.List;
 
-public class ApiError {
-
-    private final Instant timestamp = Instant.now();
-    private final int status;
-    private final String error;
-    private final List<String> messages;
+public record ApiError(Instant timestamp, int status, String error, List<String> messages) {
 
     public ApiError(int status, String error, List<String> messages) {
-        this.status = status;
-        this.error = error;
-        this.messages = messages;
-    }
-
-    public Instant getTimestamp() {
-        return timestamp;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public List<String> getMessages() {
-        return messages;
+        this(Instant.now(), status, error, messages);
     }
 }
