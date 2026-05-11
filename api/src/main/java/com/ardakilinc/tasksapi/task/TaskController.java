6a9f824758e6e426cfceca9c39e1f8dd5bb312d8
adraarda23 <ardaaydinkilinc@gmail.com>
2026-05-12 00:05:21 +0300

    feat(api): add GET /api/tasks endpoint (list all)

---
+++ api/src/main/java/com/ardakilinc/tasksapi/task/TaskController.java
@@ -0,0 +1,23 @@
+package com.ardakilinc.tasksapi.task;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    private final TaskService service;
+
+    public TaskController(TaskService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Task> list() {
+        return service.listAll();
+    }
+}
