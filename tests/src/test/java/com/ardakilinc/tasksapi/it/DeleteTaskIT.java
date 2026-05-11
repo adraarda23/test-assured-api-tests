5441e7682ca5cdf6fa61e4c74dbc9e189c869027
adraarda23 <ardaaydinkilinc@gmail.com>
2026-05-12 00:19:36 +0300

    test: add DELETE /tasks/{id} tests including 404 case

---
+++ tests/src/test/java/com/ardakilinc/tasksapi/it/DeleteTaskIT.java
@@ -0,0 +1,46 @@
+package com.ardakilinc.tasksapi.it;
+
+import com.ardakilinc.tasksapi.it.support.ResponseTimeSla;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+class DeleteTaskIT extends BaseApiIT {
+
+    @Test
+    @DisplayName("DELETE /tasks/{id} returns 204 and removes the task, under SLA")
+    void delete_returnsNoContent() {
+        Integer id = given()
+                .body("{\"title\":\"scenario-delete\",\"completed\":false}")
+            .when()
+                .post("/tasks")
+            .then()
+                .statusCode(201)
+                .extract().path("id");
+
+        given()
+            .when()
+                .delete("/tasks/{id}", id)
+            .then()
+                .statusCode(204)
+                .time(ResponseTimeSla.under());
+
+        given()
+            .when()
+                .get("/tasks/{id}", id)
+            .then()
+                .statusCode(404);
+    }
+
+    @Test
+    @DisplayName("DELETE /tasks/{id} returns 404 for unknown id")
+    void delete_unknownIdReturnsNotFound() {
+        given()
+            .when()
+                .delete("/tasks/{id}", 99_999_999L)
+            .then()
+                .statusCode(404)
+                .time(ResponseTimeSla.under());
+    }
+}
