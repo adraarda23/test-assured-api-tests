7c668afe49654c84526726f1030eaced9e7ddada
adraarda23 <ardaaydinkilinc@gmail.com>
2026-05-12 00:06:01 +0300

    feat(api): add POST /api/tasks endpoint

--- api/src/main/java/com/ardakilinc/tasksapi/task/TaskController.java
+++ api/src/main/java/com/ardakilinc/tasksapi/task/TaskController.java
@@ -3,9 +3,12 @@ package com.ardakilinc.tasksapi.task;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,5 +31,13 @@ public class TaskController {
         return service.findById(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> create(@RequestBody Task task) {
+        Task created = service.create(task);
+        return ResponseEntity
+                .created(URI.create("/api/tasks/" + created.getId()))
+                .body(created);
     }
 }
