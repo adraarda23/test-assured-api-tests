a25b495b1001a342ac1193311fd5e5fde36daf97
adraarda23 <ardaaydinkilinc@gmail.com>
2026-05-12 00:05:40 +0300

    feat(api): add GET /api/tasks/{id} endpoint

--- api/src/main/java/com/ardakilinc/tasksapi/task/TaskController.java
+++ api/src/main/java/com/ardakilinc/tasksapi/task/TaskController.java
@@ -1,6 +1,8 @@
 package com.ardakilinc.tasksapi.task;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,12 @@ public class TaskController {
     @GetMapping
     public List<Task> list() {
         return service.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getById(@PathVariable Long id) {
+        return service.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
