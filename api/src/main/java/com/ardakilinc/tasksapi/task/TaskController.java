064ba84126ab7fefebaeedf833677afd4073c8d3
adraarda23 <ardaaydinkilinc@gmail.com>
2026-05-12 00:06:19 +0300

    feat(api): add PUT /api/tasks/{id} endpoint

--- api/src/main/java/com/ardakilinc/tasksapi/task/TaskController.java
+++ api/src/main/java/com/ardakilinc/tasksapi/task/TaskController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +40,12 @@ public class TaskController {
         return ResponseEntity
                 .created(URI.create("/api/tasks/" + created.getId()))
                 .body(created);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> update(@PathVariable Long id, @RequestBody Task task) {
+        return service.update(id, task)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
